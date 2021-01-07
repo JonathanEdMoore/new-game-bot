8a23e1a1874d1f1add087008ce6b0e8bfc3eeecc
Jonathan Moore <jonathan.moore6247@gmail.com>
2021-01-06 19:04:49 -0600

    Require command to be made in create-new-game channel

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ client.on('message', function(message) {
   if(!message.content.startsWith(prefix)) {
     return
   }
+  if(message.channel.name !== 'create-new-game'){
+    message.reply(`Command must be made in the "create-new-game channel".`)
+    return
+  }
 
   const commandBody = message.content.slice(prefix.length)
   const args = commandBody.split(' ')
